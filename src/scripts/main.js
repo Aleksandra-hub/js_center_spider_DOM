d62b6834340b8ce633e69e39fccc0c17ec5de449
Oleksandra Kuziura <radioday2014@gmail.com>
2024-12-22 12:59:07 +0200

    change spider.width to spider.height

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,4 +6,4 @@ const coordsWall = wall.getBoundingClientRect();
 const coordsSpider = spider.getBoundingClientRect();
 
 spider.style.left = `${coordsWall.width / 2 - coordsSpider.width / 2}px`;
-spider.style.top = `${coordsWall.height / 2 - coordsSpider.width / 2}px`;
+spider.style.top = `${coordsWall.height / 2 - coordsSpider.height / 2}px`;
