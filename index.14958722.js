0a7a2bdc922afe1ad59b2c78f174c867c6c2f961
Oleksandra Kuziura <radioday2014@gmail.com>
2024-12-22 12:59:38 +0200

    make build

---
+++ index.14958722.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall").getBoundingClientRect(),c=e.getBoundingClientRect();e.style.left="".concat(t.width/2-c.width/2,"px"),e.style.top="".concat(t.height/2-c.height/2,"px");
+//# sourceMappingURL=index.14958722.js.map
